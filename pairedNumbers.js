d3f320bf41d8bd9c228b36884dc3944c7b3d87fe
rockshellS <spicer.rachelle@gmail.com>
2021-06-23 19:53:23 -0400

    paired function work in process

--- pairedNumbers.js
+++ pairedNumbers.js
@@ -7,7 +7,25 @@
  *  2. INTEGER_ARRAY ar
  */
 
-function sockMerchant(n, ar) {
-    // Write your code here
+// function sockMerchant(n, ar) {
+//     // Write your code here
  
+// }
+const arr = [1,1,13,4,4,5,5,5,5,6]
+
+function mostPaired(arr){
+	let nums = {}
+    const pairs = []
+	for(let x = 0; x < arr.length; x++) {
+		let elem = arr[x]
+		if (nums[elem]) {
+			// elem already in nums
+			nums[elem] += 1
+		} else {
+			// elem not in nums
+			nums[elem] = 1
+		}
+	}
+    return mostPaired()
 }
+console.log(mostPaired(arr))
