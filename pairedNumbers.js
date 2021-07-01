287aac14c029aec6ff8f89dce8d192de96a6c17f
rockshellS <spicer.rachelle@gmail.com>
2021-07-01 13:33:40 -0400

    studing pairs

--- pairedNumbers.js
+++ pairedNumbers.js
@@ -13,19 +13,36 @@
 // }
 
 
-const arr = [1,4,4,4,4,1,1,3,2,2,6,6,6,6,7]
-const n = 15
-const sortAndCount = (n, arr) => {
+// const arr = [1,4,4,4,4,1,1,3,2,2,6,6,6,6,7]
+// const n = 15
+// const sortAndCount = (n, arr) => {
+//     let pairs = 0;
+//     // let numsSorted = arr.sort( (a,b) => a-b);
+//     for (let i = 0; i < n - 1; i++ ) {
+//         if (arr[i] === arr[i + 1]) {
+//             pairs++;
+//             i += 1;
+//         }
+        
+//         }
+//     return pairs
+// }
+
+// console.log(`Total: ${sortAndCount(n , arr)} pairs`)
+
+
+const arr = [10,20,20,10,10,30,50,10,20]
+const n = 9
+const sockMerchant = (n, arr) => {
     let pairs = 0;
-    let numsSorted = arr.sort( (a,b) => a-b);
-    for (let i = 0; i < n - 1; i++ ) {
-        if (numsSorted[i] === numsSorted[i + 1]) {
+     let numsSorted = arr.sort( (a,b) => a-b);
+    for (let i = 0; i < n - 1; i++) {
+        if (arr[i] === arr[i + 1]) {
             pairs++;
             i += 1;
         }
-        
-        }
+    }
     return pairs
 }
 
-console.log(`Total: ${sortAndCount(n , arr)} pairs`)
+console.log(`Total: ${sockMerchant(n,arr)} pairs`)
