9230aca1e4d8cfe9a43e45bf73c3c87e1a065ded
rockshellS <spicer.rachelle@gmail.com>
2021-07-07 15:12:32 -0400

    edabit pratice

---
+++ edabit.js
@@ -0,0 +1,32 @@
+
+// const canNest = (arr1, arr2) => {
+//     if  (Math.min(...arr1) > Math.min(...arr2) && Math.max(...arr1) < Math.max(...arr2)){
+//         return true
+//     }else
+//        return false
+    
+// }
+
+// console.log(canNest([1,2,3,4], [0,6]))
+
+
+
+//Write a function that takes a two-digit number and determines if it's the largest of two possible digit swaps.
+// largestSwap(27) ➞ false
+
+// largestSwap(43) ➞ true
+
+// const largeSwap = (num) => {
+//     // return num >= parseInt([...(num + "")].reverse().join(""))
+//     // return num / 10 > num % 10
+//     return num >= num.reverse
+// }
+// console.log(largeSwap(32))
+
+
+
+//Create a function that calculates the number of different squares in an n * n square grid.
+const nGrid = (n) => {
+   n **2
+}
+console.log(2)
