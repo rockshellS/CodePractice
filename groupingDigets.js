46b08864a6feb248ec88d8004ea145e16e835a27
rockshellS <spicer.rachelle@gmail.com>
2021-07-15 15:07:57 -0400

    twitter

--- groupingDigets.js
+++ groupingDigets.js
@@ -66,6 +66,76 @@
 
 
 
+// arr = [1,1,1,1,0,1,0,1]
+
+// function miniMoves(arr) {
+
+// let count = 0
+//     // this decides how many numbers to check
+//     for(let i = 0; i < arr.length; i++) {
+//         // this one does the actual checking
+//         for(let x = 0; x < (arr.length - i - 1); x++) {
+//             //compare numbers and only swap if if the second one is smaller
+//             if(arr[x] > arr[x + 1]) {
+//                 let first = arr[x] // store the first number
+//                 let second = arr[x + 1] // store the second number
+//                 // now swap them
+//                 arr[x] = second
+//                 arr[x + 1] = first
+//                 count++
+//             }
+//         }
+//     }
+//     return count
+// }
+// console.log(miniMoves(arr))
+
+
+// just swap if different not less than
+
+arr = [1,0,1,0,0,0,0,1]
+
+function miniMoves(arr) {
+
+let count = 0
+    // this decides how many numbers to check
+    for(let i = 0; i < arr.length; i++) {
+        // this one does the actual checking
+        for(let x = 0; x < (arr.length - i - 1); x++) {
+            //compare numbers and only swap if if the second one is larger
+            if(arr[x] < arr[x + 1]) {
+                
+                let first = arr[x] // store the first number
+                let second = arr[x + 1] // store the second number
+                // now swap them
+                arr[x] = second
+                arr[x + 1] = first
+                count++
+            }
+        }
+    }
+    return count
+}
+console.log(miniMoves(arr))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // arr = [1,0,1,0,0,0,0,1]
 
@@ -95,20 +165,68 @@
 // console.log(miniMoves(arr))
 
 
-arr = [1,0,1,0,0,0,0,1]
+// arr = [1,1,1,1,0,1,0,1]
 
-function miniMoves(arr) {
-    count1 = 0
-    dis = 0
+// function miniMoves(arr) {
+//     let count1 = 0
+//     let onesCount1 = 0
 
-    for( let i = 0; i < arr.length; i ++) {
-        if (arr[i] === 1) {
-            count1++
-        }
-        if (arr[i] === 0) {
-            dis++
-        }
-    }
+//     for( let i = 0; i < arr.length; i ++) {
+//         if (arr[i] === 1) {
+//             onesCount1++
+//         }else{
+//             count1 += onesCount1
+//         }
+//     }
+ 
+//     let count2 = 0
+//     let onesCount2 = 0
 
-    }
-}
+//     for (let x = 0; x < arr.length; x++ ) {
+//         if (arr[x] === 1) {
+//             onesCount2++
+//         }else {
+//             count2 += onesCount2
+//         }
+//     }
+//     // return count1 < count2 ? "count1" : "count2"
+//     return count2
+
+
+     
+// }
+// console.log(miniMoves(arr))
+    
+
+// arr = [1,1,1,1,0,1,0,1]
+// function minSwaps(arr) {
+
+//     // keep track of zeroes for FIRST loop
+//     let count_1 = 0;
+//     let zeros_1 = 0;
+//     // loop starting from back
+//       for (let i = arr.length - 1; i >= 0; i--) {
+//       if (arr[i] === 0) {
+//         zeros_1 += 1;
+//       } else {
+//         count_1 += zeros_1;
+//       }
+//     }
+      
+//     // keep track of zeroes for SECOND loop
+//     let count_2 = 0;
+//     let zeros_2 = 0;
+      
+//     // start looping from front
+//     for (let j = 0; j < arr.length; j++) {
+//       if (arr[j] === 0) {
+//         zeros_2 += 1;
+//       } else {
+//         count_2 += zeros_2;
+//       }
+//     }
+//       // return which ever count is smaller
+//     // return count_1 < count_2 ? 'count_1' : 'count_2'
+//     return count_2
+//    }
+//    console.log(minSwaps(arr))
